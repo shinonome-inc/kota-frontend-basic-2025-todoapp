f73cafed8fb227ba1be4ed6fa02a808a7eb82177
harada-0 <k.harada.25.2.1660@4nonome.com>
2025-03-29 14:13:50 +0900

    sbファイルのimport文の変更

--- src/components/Molecules/index.stories.jsx
+++ src/components/Molecules/index.stories.jsx
@@ -1,11 +1,11 @@
-import { Task } from "./index";
+import Task from "./index";
 
 export default { component: Task };
 
 export const Default = {
     args: {
         onTaskNameChange: (value) => {
-            console.log("taskname changed:", value);
+            console.log("taskName changed:", value);
         },
         onTaskComplete: () => {
             console.log("task completed");
