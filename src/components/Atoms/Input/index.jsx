7dc3d2387b01bc2f5eedcde6bd161a6c59f85c0c
harada-0 <k.harada.25.2.1660@4nonome.com>
2025-03-31 11:53:17 +0900

    focus時の動作に対するcssの変更

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -2,46 +2,47 @@ import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import TEXT from "../../../variables/texts";
+import FONTFAMILY from "../../../variables/font_family";
 
 export default function Input({ onEditComplete, defaultValue }) {
-  
-    const inputRef = useRef(null);
-  
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  const inputRef = useRef(null);
 
-    const handleBlur = () => {
-        onEditComplete(inputRef.current.value);
-    };
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            onEditComplete(inputRef.current.value);
-        }
-    };
-  
-    return (
-        <StyledInput 
-            ref={inputRef}
-            defaultValue={defaultValue}
-            onBlur={handleBlur} 
-            onKeyDown={handleKeyDown}
-        />
-    );
+  const handleBlur = () => {
+    onEditComplete(inputRef.current.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onEditComplete(inputRef.current.value);
+    }
+  };
+
+  return (
+    <StyledInput
+      ref={inputRef}
+      defaultValue={defaultValue}
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+    />
+  );
 }
 
 const StyledInput = styled.input`
-    display: flex;
-    width: 100%;
-    padding: 0 4px;
-    background: ${COLOR.BLACK};
-    border: none;
-    border-radius: 2px;
-    color: ${COLOR.LIGHT_GRAY};
-    gap: 10px;
-    ${TEXT.S}
-    &:focus {
-        outline: none;
-    }
+  display: flex;
+  width: 100%;
+  padding: 0 4px;
+  background: ${COLOR.BLACK};
+  border: none;
+  border-radius: 2px;
+  color: ${COLOR.LIGHT_GRAY};
+  gap: 10px;
+  ${TEXT.S}
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  &:focus {
+    outline: none;
+  }
 `;
